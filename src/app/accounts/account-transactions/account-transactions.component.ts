7870f5639f985881ed2164bcb25cf882bf3b3112
Vijay Bajrot <vijay.bajrot@ongraphmail.com>
2021-07-19 15:09:55 +0530

    Updates

--- src/app/accounts/account-transactions/account-transactions.component.ts
+++ src/app/accounts/account-transactions/account-transactions.component.ts
@@ -1,5 +1,6 @@
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Account, AccountService, Transaction } from '../account.service';
 
 @Component({
@@ -13,21 +14,29 @@ export class AccountTransactionsComponent implements OnInit {
 
   constructor(
     private accountService: AccountService,
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.activeRoute.params.subscribe((params) => {
       const accountId = params.id;
 
-      this.accountService
-        .getAccount(accountId)
-        .subscribe((account: Account) => {
+      this.accountService.getAccount(accountId).subscribe(
+        (account: Account) => {
           this.account = account;
           this.accountService
             .getAccountTransactions(accountId)
             .subscribe((transactions) => (this.transactions = transactions));
-        });
+        },
+        (error: HttpErrorResponse) => {
+          if (error.status === HttpStatusCode.NotFound) {
+            return this.router.navigate(['/']);
+          }
+
+          throw error;
+        }
+      );
     });
   }
 }
